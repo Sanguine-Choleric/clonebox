28f696451ca3b7ca6f1ffda50bb987efa22e9bd6
Sanguine-Choleric <130211779+Sanguine-Choleric@users.noreply.github.com>
2024-12-26 16:02:41 -0800

    CSRF bug fix

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -41,7 +41,7 @@ type application struct {
 
 func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address")
-	dsn := flag.String("dsn", "web:asdfjkl;asdfjkl@/snippetbox?parseTime=true", "MySQL data source name")
+	dsn := flag.String("dsn", "web:pass@/snippetbox?parseTime=true", "MySQL data source name")
 
 	flag.Parse()
 
