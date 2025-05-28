5e00698cbff619f1b874c377b4a5b35563a08667
Sanguine-Choleric <130211779+Sanguine-Choleric@users.noreply.github.com>
2025-05-27 19:08:16 -0700

    Dockerfile and compose updates and cleanup.
    - Commenting out delve setup for now; remote debugging too annoying. Need a better way to switch between setups.
    - Dockerfile separates go.mod/go.sum copying for better caching (~1-2s build improvement)

--- compose/sql/bak/7 - link_mapping.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE link_mapping
-(
-    id            INTEGER      NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    original_link VARCHAR(100) NOT NULL UNIQUE,
-    short_link    VARCHAR(100) NOT NULL UNIQUE
-);
-
